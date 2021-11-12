8776c12f5ae0e0daa451a932c342353a05398488
Guilherme Illescas <gui.illescas@gmail.com>
2021-11-12 14:30:43 -0300

    Created page styles

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    ::-webkit-scrollbar {
+      width: 0.4rem;
+      height: 0.2rem;
+      margin-right: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      /* background-color: var(--scroll); */
+      border-radius: 3px;
+      cursor: move;
+    }
+  }
+
+  @font-face {
+    font-family: 'Ubuntu';
+    src: url('../fonts/Ubuntu/Ubuntu-Light.ttf') format('ttf');
+    font-style: normal;
+    font-weight: 300;
+    font-display: fallback; /* <- this can be added to each @font-face definition */
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    font-family: 'Ubuntu', sans-serif;
+    color: ${({ theme }) => theme.colors.text};
+    overflow-x: hidden;
+
+    transition: background 0.2s;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
