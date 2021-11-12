4af5ed3ecd2b766b068b43c2ad64074a1ad5ed5c
Guilherme Illescas <gui.illescas@gmail.com>
2021-11-12 14:30:24 -0300

    Created theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  colors: {
+    primary: '#ffd500',
+    secondary: '#00509D',
+    background: '#f7f7ff',
+    white: '#fff',
+    black: '#000',
+    text: '#363636',
+    success: '#83c5be',
+    error: '#fe5f55',
+  },
+}
