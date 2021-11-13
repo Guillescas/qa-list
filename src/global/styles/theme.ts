d83fee6f1c79bfa6693a1295e03c3ac398c65096
Guilherme Illescas <gui.illescas@gmail.com>
2021-11-13 01:23:56 -0300

    Increase theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -3,11 +3,12 @@ export default {
   colors: {
     primary: '#ffd500',
     secondary: '#00509D',
-    background: '#f7f7ff',
+    background: '#d9d9d9',
     white: '#fff',
     black: '#000',
     text: '#363636',
     success: '#83c5be',
     error: '#fe5f55',
+    disabled: '#c5c3c6'
   },
 }
